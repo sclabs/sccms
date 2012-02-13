4bc413a317d26abcd04de63c8529a8014576fbdc
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-02-12 19:12:41 -0500

    more typo tweaking

--- settings.py
+++ settings.py
@@ -11,13 +11,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# Force lighttpd to get the {% url %} tags right
-FORCE_SCRIPT_NAME = ''
-
 DATABASES = {
     'default': {
-        'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
