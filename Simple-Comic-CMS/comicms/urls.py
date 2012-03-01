6994ad9c2f508520b7ea03b5d0ed347498ac89c0
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-03-01 00:17:07 -0500

    created separate urls.py for comicms app

---
+++ Simple-Comic-CMS/comicms/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('comicms.views',
+    url(r'^$', 'lastcomic', name="last"),
+    url(r'^(\d+)/$', 'particularcomic', name="particular"),
+    url(r'^random/$', 'randomcomic', name="random"),
+    url(r'^(\d+)/previous/$', 'previouscomic', name="previous"),
+    url(r'^(\d+)/next/$', 'nextcomic', name="next"),
+)
--- Simple-Comic-CMS/urls.py
+++ Simple-Comic-CMS/urls.py
@@ -1,23 +1,12 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 from settings import DEBUG, MEDIA_ROOT, STATIC_ROOT
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'comicms.views.lastcomic', name="last"),
-    url(r'^(\d+)/$', 'comicms.views.particularcomic', name="particular"),
-    url(r'^random/$', 'comicms.views.randomcomic', name="random"),
-    url(r'^(\d+)/previous/$', 'comicms.views.previouscomic', name="previous"),
-    url(r'^(\d+)/next/$', 'comicms.views.nextcomic', name="next"),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('comicms.urls')),
 )
 
 if DEBUG:
